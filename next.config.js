8290f76edfce3c89df8ccea2fa1ea58730845d9a
SuperationBn <superationbn@gmail.com>
2022-09-02 19:02:24 -0400

    p v0.2

--- next.config.js
+++ next.config.js
@@ -8,5 +8,17 @@ module.exports = {
   images: {
     domains: ['images.asos-media.com', 'images.pexels.com'],
   },
-  nextConfig
+  build: {
+    command: 'npm run build',
+    commandOrigin: 'ui',
+    environment: '- NEXT_PRIVATE_TARGET',
+    publish: '/opt/build / repo /.next',
+    publishOrigin: 'ui',
+    nextConfig
+  },
+  plugins: {
+    '-inputs': {},
+    origin: 'ui',
+    package: '@netlify/plugin-nextjs'
+  }
 }
